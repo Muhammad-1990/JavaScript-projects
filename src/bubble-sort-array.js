1af6192a899000f259501cbf2a02250e90b07fad
Muhammad Ahmod <53302444+Muhammad-1990@users.noreply.github.com>
2024-01-05 22:55:41 +0200

    Update bubble-sort-array.js

---
+++ src/bubble-sort-array.js
@@ -0,0 +1,13 @@
+// Function to sort areay in ascending order.
+
+function sort(arr){
+    // Logic to sort arr
+    // ...
+    return arr
+}
+
+var myarr = [5,3,7,2,9,0];
+
+var sortedArr = sort(myarr);
+
+console.log(sortedAre);
